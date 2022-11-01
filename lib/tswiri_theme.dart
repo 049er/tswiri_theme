927d2ab23ee6af08920afc13c832bf7441918908
049er <werner@scholtzonline.net>
2022-11-01 09:57:35 +0200

    Color changes

--- lib/tswiri_theme.dart
+++ lib/tswiri_theme.dart
@@ -6,20 +6,26 @@ import 'package:tswiri_theme/colors/light_theme_colors.g.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 ThemeData darkThemeData = FlexThemeData.dark(
-  scheme: FlexScheme.blueWhale,
-  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-  blendLevel: 15,
-  appBarOpacity: 0.90,
-  appBarElevation: 1.0,
-  subThemesData: const FlexSubThemesData(
-    blendOnLevel: 30,
-    fabSchemeColor: SchemeColor.secondaryContainer,
-  ),
-  visualDensity: FlexColorScheme.comfortablePlatformDensity,
-  useMaterial3: true,
-  // To use the playground font, add GoogleFonts package and uncomment
-  // fontFamily: GoogleFonts.notoSans().fontFamily,
-);
+    scheme: FlexScheme.blueWhale,
+    surfaceMode: FlexSurfaceMode.level,
+    blendLevel: 15,
+    appBarOpacity: 0.90,
+    appBarElevation: 1.0,
+    darkIsTrueBlack: true,
+    surfaceTint: Color(0xffb6cad4),
+    subThemesData: const FlexSubThemesData(
+      blendOnLevel: 30,
+      switchSchemeColor: SchemeColor.secondaryContainer,
+      checkboxSchemeColor: SchemeColor.secondaryContainer,
+      radioSchemeColor: SchemeColor.secondaryContainer,
+      inputDecoratorRadius: 12.0,
+      fabSchemeColor: SchemeColor.secondaryContainer,
+      // navigationBarSelectedLabelSchemeColor: SchemeColor.onSecondaryContainer,
+      // navigationBarSelectedIconSchemeColor: SchemeColor.onSecondaryContainer,
+      // navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    useMaterial3: true);
 // ThemeData(
 //   useMaterial3: true,
 //   brightness: Brightness.dark,
@@ -40,12 +46,17 @@ ThemeData lightThemeData = FlexThemeData.light(
   subThemesData: const FlexSubThemesData(
     blendOnLevel: 20,
     blendOnColors: false,
+    switchSchemeColor: SchemeColor.secondaryContainer,
+    checkboxSchemeColor: SchemeColor.secondaryContainer,
+    radioSchemeColor: SchemeColor.secondaryContainer,
+    inputDecoratorRadius: 12.0,
     fabSchemeColor: SchemeColor.secondaryContainer,
+    // navigationBarSelectedLabelSchemeColor: SchemeColor.onSecondaryContainer,
+    // navigationBarSelectedIconSchemeColor: SchemeColor.onSecondaryContainer,
+    // navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
   ),
   visualDensity: FlexColorScheme.comfortablePlatformDensity,
   useMaterial3: true,
-  // To use the playground font, add GoogleFonts package and uncomment
-  // fontFamily: GoogleFonts.notoSans().fontFamily,
 );
 
 
